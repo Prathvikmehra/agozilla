eb540422cadcc132f3412f70618b0495cb848148
prathvik mehra <prathvik.mehra.cg@gmail.com>
2026-06-26 22:28:14 +0530

    feat: add until API

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require("./lib/fromNow");
+const fromNow = require("./lib/fromNow");
+
+const duration = require("./lib/duration");
+const until = require("./lib/until");
+const smart = require("./lib/smart");
+const calendar = require("./lib/calendar");
+const countdown = require("./lib/countdown");
+
+// Backward compatibility
+fromNow.fromNow = fromNow;
+
+// New APIs
+fromNow.duration = duration;
+fromNow.until = until;
+fromNow.smart = smart;
+fromNow.calendar = calendar;
+fromNow.countdown = countdown;
+
+module.exports = fromNow;
